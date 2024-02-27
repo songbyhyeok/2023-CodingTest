4d78ac97a5102102181a1dcc514675260b038318
WhiteBear <songbyhyeok@gmail.com>
2024-02-27 14:38:02 +0900

    49. Group Anagrams

---
+++ leetcode/String/49. Group Anagrams/Solution.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> answer = new HashMap<>();
+
+        return new ArrayList<>(answer.values());
+    }
+}
